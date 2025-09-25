6d87332238bb74e53540a10d99eb42a4ec89da3e
Miller <2311067@mail.nankai.edu.cn>
2025-09-25 20:59:30 +0800

    add leetcode 410 c version
    this is a typical "first true" example of Binary Search Algrithm

---
+++ 410.分割数组的最大值.c
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=410 lang=c
+ *
+ * [410] 分割数组的最大值
+ */
+#include <math.h>
+// @lc code=start
+int splitArray(int* nums, int numsSize, int k) {
+    int left=0;
+    int right=0;
+    for (int i=0;i<numsSize;i++){
+        if (nums[i]>left){
+            left=nums[i];
+        }
+        right+=nums[i];
+    }
+
+
+    while (left<right){
+        int mid=left+(right-left)/2;
+        int count=1;
+        int sum=0;
+        for (int i=0;i<numsSize;i++){
+            sum+=nums[i];
+            if (sum>mid){
+                count+=1;
+                sum=nums[i];
+            }
+            if (count>k) break;
+        }
+        if (count>k){
+            left=mid+1;
+        } else{
+            right=mid;
+        }
+    }
+    return left;
+
+}
+// @lc code=end
+
